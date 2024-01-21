fa0b031243ab25e32890f0d7451387ec15e74702
Dil <126651350+dilop121@users.noreply.github.com>
2024-01-21 21:15:17 +0530

    Update start.py

--- DILXHACK/plugins/start.py
+++ DILXHACK/plugins/start.py
@@ -1,4 +1,7 @@
 import env
+import os
+import random
+from telethon.tl.types import InputMediaPhoto
 from DILXHACK import bot
 from DILXHACK.helpers import MENU1, KEYBOARD1
 from DILXHACK.database import DB
@@ -11,12 +14,28 @@ async def start(event):
     id = event.sender_id
     mention = f"[{event.sender.first_name}](tg://user?id={id})"
     TEXT = "Hey {}, I am a Session Hacker Bot Supporting Both Pyrogram and Telethon Session String. Type /hack to see menu"
-    await event.reply(TEXT.format(mention))
+    
+    buttons = [
+        [Button.inline("Owner", data="owner"), Button.inline("Support", data="support")],
+    ]
+    
+    photo_urls = [
+        "https://graph.org/file/37077de233a43b6da9cee.jpg",
+        "https://graph.org/file/acb802ec1b54e02d4e692.jpg",
+        "https://graph.org/file/2e84b98fe4bdb0b53807e.jpg",
+        "https://graph.org/file/8883ddb847f34cdf0420d.jpg",
+    ]
+    
+    random_photo_url = random.choice(photo_urls)
+    
+    await bot.send_message(event.chat_id, TEXT.format(mention), buttons=buttons, file=random_photo_url)
+    
     if DB:
         await DB.add_user(id)
+    
     if env.LOG_GROUP_ID:
-        await bot.send_message(env.LOG_GROUP_ID,
-                               f'{mention} Has Just Started The Bot')
+        await bot.send_message(env.LOG_GROUP_ID, f'{mention} Has Just Started The Bot')
+
 
 
 @bot.on(events.NewMessage(pattern="/hack"))
